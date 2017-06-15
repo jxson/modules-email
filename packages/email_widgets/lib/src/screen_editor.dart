8451fcc277cd94783da56400066e0355655e558e
Alhaad Gokhale <alhaad@google.com>
2017-06-14 17:23:08 -0700

    Fix EditorScreen state to always update recipientList, subject, text.
    This is required because the EmailComposerModuleModel does not update
    its _message member object anymore.
    
    Change-Id: I91f6739cb4202dd4b76f19f29cf2435bf29117d1

--- packages/email_widgets/lib/src/screen_editor.dart
+++ packages/email_widgets/lib/src/screen_editor.dart
@@ -102,11 +102,9 @@ class _EditorScreenState extends State<EditorScreen> {
   @override
   void didUpdateWidget(EditorScreen oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (oldWidget.draft != widget.draft) {
-      _recipientList = widget.draft.recipientList;
-      _subject = widget.draft.subject;
-      _text = widget.draft.text;
-    }
+    _recipientList = widget.draft.recipientList;
+    _subject = widget.draft.subject;
+    _text = widget.draft.text;
   }
 
   void _handleRecipientChange(List<Mailbox> recipientList) {
