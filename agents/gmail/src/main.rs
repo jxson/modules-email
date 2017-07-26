9d0ff9a963e19e08cc388e1c0c27e0492d308b26
Jason Campbell <jasoncampbell@google.com>
2017-07-26 14:12:37 -0700

    Build boilerplate
    Change-Id: Iaf0ef6a8f04272df994214567bf85a001d96644b

---
+++ agents/gmail/src/main.rs
@@ -0,0 +1,8 @@
+
+pub fn main() {
+    println!("# starting...");
+    //   _agent = new EmailContentProviderAgent(
+    //     applicationContext: new ApplicationContext.fromStartupInfo(),
+    //   );
+    //   _agent.advertise();
+}
